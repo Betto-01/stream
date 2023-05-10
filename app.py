7d7e72c6bf8430d263f1f70a5922fa478dea8174
lucabetto <salamoumayma@gmail.com>
2023-05-10 12:12:41 +0200

    iris.pkl

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
+import xlsxwriter
+import joblib 
+def main():
+    st.title("Iris prediction")
+    model = joblib.load('iris.pkl')
+    sl = st.number_input("inserisci il valore per sepal lenght",1,10,3)
+    sw = st.number_input("inserisci il valore per sepal width",1,10,3)
+    pl = st.number_input("inserisci il valore per petal lenght",1,10,3)
+    pw = st.number_input("inserisci il valore per petal width",1,10,3)
+    res = model.predict([[3,3,3,3]])[0]
+    if st.button('predict'):
+        st.write(res)
 
-st.text('ciao scre') #non fa nulla da solo, bisogna fare streamlit run app.py sul terminale
-st.text('salve a tutti')
+
+
+if __name__ == "__main__":
+    main()
